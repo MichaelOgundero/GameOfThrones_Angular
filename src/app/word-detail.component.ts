6b276486832b2f1c21a4f374ef40fa9a54cfb16f
youngTickles <ogunderomichael@gmail.com>
2019-04-22 20:54:55 +0200

    made some changes to the pages

--- src/app/word-detail.component.ts
+++ src/app/word-detail.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './word-detail/word-detail.component.html',
-  styleUrls: [ './word-detail/word-detail.component.css' ]
+  templateUrl: './word-detail.component.html',
+  styleUrls: [ './word-detail.component.css' ]
 })
 export class WordDetailComponent {}
 
--- src/app/word-search.component.ts
+++ src/app/word-search.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './word-search/word-search.component.html',
-  styleUrls: [ './word-search/word-search.component.css' ]
+  templateUrl: './word-search.component.html',
+  styleUrls: [ './word-search.component.css' ]
 })
 export class WordSearchComponent {}
